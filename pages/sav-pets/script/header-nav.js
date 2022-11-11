348467fb952c18bb9c3ef5e4762ffac379acc3ef
yllaciarbegla <yllaciarbegla@gmail.com>
2022-11-11 19:33:52 +0200

    some fixed
    fixed body lock on mobile and button sizes; choose-way: flex => grid

--- pages/sav-pets/script/header-nav.js
+++ pages/sav-pets/script/header-nav.js
@@ -1,7 +1,7 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
 menuBtn.addEventListener('click', function () {
-	document.body.classList.toggle('_lock');
+	document.body.classList.toggle('lock');
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
 })
