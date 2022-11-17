3b4a42dcaf958aa6acfa3abdbf4c625a5bcb6cd2
yllaciarbegla <yllaciarbegla@gmail.com>
2022-11-17 19:27:59 +0200

    update beautiful-world
    added loader to page

--- pages/beautiful-world/src/js/app.js
+++ pages/beautiful-world/src/js/app.js
@@ -1,91 +1,97 @@
+window.onload = function () {
+	setTimeout(function () {
+		document.body.classList.add("loaded");
+	}, 500)
+}
+
 // 3D Scroll
 
 let zSpacing = -2000;
-let =	lastPoz = zSpacing / 5;
-let =	$frames = document.getElementsByClassName('frame');
-let =	frames = Array.from($frames);
-let =	zVals = [];
+	let = lastPoz = zSpacing / 5;
+	let = $frames = document.getElementsByClassName('frame');
+	let = frames = Array.from($frames);
+	let = zVals = [];
 
-window.onscroll = function () {
+	window.onscroll = function () {
 
-	let top = document.documentElement.scrollTop;
-	let	delta = lastPoz - top
+		let top = document.documentElement.scrollTop;
+		let delta = lastPoz - top
 
-	lastPoz = top;
+		lastPoz = top;
 
-	frames.forEach(function (currentEl, indexEl) {
-		zVals.push((indexEl * zSpacing) + zSpacing);
-		zVals[indexEl] += delta * -3;
-		let frame = frames[indexEl];
-		let	transform = `translateZ(${zVals[indexEl]}px)`;
-		let opacity = zVals[indexEl] < Math.abs(zSpacing) / 1.8 ? 1 : 0
-		frame.setAttribute('style', `transform: ${transform}; opacity: ${opacity}`)
+		frames.forEach(function (currentEl, indexEl) {
+			zVals.push((indexEl * zSpacing) + zSpacing);
+			zVals[indexEl] += delta * -3;
+			let frame = frames[indexEl];
+			let transform = `translateZ(${zVals[indexEl]}px)`;
+			let opacity = zVals[indexEl] < Math.abs(zSpacing) / 1.8 ? 1 : 0
+			frame.setAttribute('style', `transform: ${transform}; opacity: ${opacity}`)
+		})
+
+	}
+
+	window.scrollTo(0, 1);
+
+	// Audio
+
+	let soundButton = document.querySelector('.soundbutton');
+	let audio = document.querySelector('.audio');
+
+	soundButton.addEventListener('click', () => {
+		soundButton.classList.toggle('paused');
+		audio.paused ? audio.play() : audio.pause();
 	})
 
-}
+	window.onfocus = () => {
+		soundButton.classList.contains('paused') ? audio.pause() : audio.play();
+	}
 
-window.scrollTo(0, 1);
+	window.onblur = () => {
+		audio.pause();
+	}
 
-// Audio
+	// Cursor
 
-let soundButton = document.querySelector('.soundbutton');
-let audio = document.querySelector('.audio');
+	let aura = document.querySelector('.aura');
+	let cursor = document.querySelector('.cursor');
 
-soundButton.addEventListener('click', () => {
-	soundButton.classList.toggle('paused');
-	audio.paused ? audio.play() : audio.pause();
-})
+	document.addEventListener('mousemove', function (e) {
+		let x = e.clientX;
+		let y = e.clientY;
+		aura.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+	});
 
-window.onfocus = () => {
-	soundButton.classList.contains('paused') ? audio.pause() : audio.play();
-}
+	document.addEventListener('mousemove', function (e) {
+		let x = e.clientX;
+		let y = e.clientY;
+		cursor.style.left = x + 'px';
+		cursor.style.top = y + 'px';
+	});
 
-window.onblur = () => {
-	audio.pause();
-}
+	document.addEventListener('mousedown', function () {
+		cursor.classList.add('cursorinnerhover');
+	});
 
-// Cursor
+	document.addEventListener('mouseup', function () {
+		cursor.classList.remove('cursorinnerhover');
+	});
 
-let aura = document.querySelector('.aura');
-let cursor = document.querySelector('.cursor');
+	document.addEventListener('mousemove', function () {
+		cursor.classList.remove('hidden');
+		aura.classList.remove('hidden');
+	});
 
-document.addEventListener('mousemove', function (e) {
-	let x = e.clientX;
-	let y = e.clientY;
-	aura.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
-});
+	document.addEventListener('mouseout', function () {
+		cursor.classList.add('hidden');
+		aura.classList.add('hidden');
+	});
 
-document.addEventListener('mousemove', function (e) {
-	let x = e.clientX;
-	let y = e.clientY;
-	cursor.style.left = x + 'px';
-	cursor.style.top = y + 'px';
-});
+	soundButton.addEventListener('mousemove', function () {
+		cursor.classList.add('active');
+		aura.classList.add('active');
+	});
 
-document.addEventListener('mousedown', function () {
-	cursor.classList.add('cursorinnerhover');
-});
-
-document.addEventListener('mouseup', function () {
-	cursor.classList.remove('cursorinnerhover');
-});
-
-document.addEventListener('mousemove', function () {
-	cursor.classList.remove('hidden');
-	aura.classList.remove('hidden');
-});
-
-document.addEventListener('mouseout', function () {
-	cursor.classList.add('hidden');
-	aura.classList.add('hidden');
-});
-
-soundButton.addEventListener('mousemove', function () {
-	cursor.classList.add('active');
-	aura.classList.add('active');
-});
-
-soundButton.addEventListener('mouseout', function () {
-	cursor.classList.remove('active');
-	aura.classList.remove('active');
-});
+	soundButton.addEventListener('mouseout', function () {
+		cursor.classList.remove('active');
+		aura.classList.remove('active');
+	});
