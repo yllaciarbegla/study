c6a019cb6ae933f91cda3b63d6d9e61893d91d4a
yllaciarbegla <yllaciarbegla@gmail.com>
2022-11-22 14:26:03 +0200

    rename function

--- pages/sav-pets/script/header-colored.js
+++ pages/sav-pets/script/header-colored.js
@@ -5,13 +5,13 @@ const headerImage = document.querySelector('.header__logo-image');
 let menuButton = document.querySelector('.menu-btn');
 
 const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
-const containHide = () => header.classList.contains('bg_colored');
+const containColored = () => header.classList.contains('bg_colored');
 
 window.addEventListener('scroll', () => {
-	if (scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset) {
+	if (scrollPosition() > lastScroll && !containColored() && scrollPosition() > defaultOffset) {
 		header.classList.add('bg_colored');
 		headerImage.src = 'images/icon/white-logo.svg';
-	} else if (scrollPosition() == 0 && containHide()) {
+	} else if (scrollPosition() == 0 && containColored()) {
 		header.classList.remove('bg_colored');
 		headerImage.src = 'images/icon/blue-logo.svg';
 	}
